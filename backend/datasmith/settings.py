280f7072dee274cdd2101d42ef319251fa749da3
Danylo Svyryd <dantessvirid@gmail.com>
2024-11-06 02:27:26 +0100

    backend url fix

--- backend/datasmith/settings.py
+++ backend/datasmith/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ct^hw9th+k)qk+rzad&lb1zz2u8gercl+7_ad=c_i(qoh&he5d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
