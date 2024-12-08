d31c3ea2fe0a02eceda704e836d0d1f4b345f40e
Danylo Svyryd <dantessvirid@gmail.com>
2024-12-09 00:11:51 +0100

    Minimal styling done

--- frontend/src/components/DatabaseForm.js
+++ frontend/src/components/DatabaseForm.js
@@ -98,140 +98,152 @@ const DataBaseForm = () => {
 
     return (
         <div className="container">
-            <h1>Data Source Management</h1>
+            <div className="row">
 
-            {/* List Data Sources */}
-            <section>
-                <h2>Available Data Sources</h2>
-                <ul>
-                    {dataSources.map((ds) => (
-                        <li key={ds.id}>
-                            {ds.name} ({ds.type})
-                            <button
-                                onClick={() => setSelectedDataSource(ds.id)}
-                                className={selectedDataSource === ds.id ? "btn btn-primary" : "btn btn-secondary"}
-                            >
-                                Select
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </section>
+                <h1>Data Source Management</h1>
 
-            {/* Test Connection Form */}
-            <section>
-                <h2>Test Data Source Connection</h2>
-                <form onSubmit={handleSubmit(handleTestConnection)}>
-                    <div>
-                        <label>Data Source Name</label>
-                        <input
-                            {...register("name", { required: true })}
-                            type="text"
-                            placeholder="Data Source Name"
-                        />
-                    </div>
-                    <div>
-                        <label>Type</label>
-                        <select {...register("type", { required: true })}>
-                            <option value="POSTGRES">PostgreSQL</option>
-                            <option value="MYSQL">MySQL</option>
-                            <option value="sqlite">SQLite</option>
-                        </select>
-                    </div>
-                    
-                    <div>
-                        <label>Host</label>
-                        <input
-                            {...register("credentials.host", { required: true })}
-                            type="text"
-                            placeholder="Host"
-                        />
-                    </div>
-                    <div>
-                        <label>Port</label>
-                        <input
-                            {...register("credentials.port", { required: true })}
-                            type="number"
-                            placeholder="Port"
-                        />
-                    </div>
-                    <div>
-                        <label>Description</label>
-                        <input
-                            {...register("description", { required: true })}
-                            type="text"
-                            placeholder="Description"
-                        />
-                    </div>
-                    <div>
-                        <label>Database</label>
-                        <input
-                            {...register("credentials.database", { required: true })}
-                            type="text"
-                            placeholder="Database"
-                        />
-                    </div>
-                    <div>
-                        <label>Username</label>
-                        <input
-                            {...register("credentials.user", { required: true })}
-                            type="text"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div>
-                        <label>Password</label>
-                        <input
-                            {...register("credentials.password", { required: true })}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </div>
+                {/* List Data Sources */}
 
-                    {/* Buttons for Different Actions */}
-                    <div className="button-group">
-                        <button type="button" onClick={handleCancel}>
-                            Cancel
-                        </button>
-                        <button type="button" onClick={handleSubmit(handleStoreConnection)}>
-                            Store
-                        </button>
-                        <button type="button" onClick={handleSubmit(handleTestConnection)}>
-                            Test
-                        </button>
-                        <button type="button" onClick={handleConnect}>
-                            Connect
-                        </button>
+
+                {/* Test Connection Form */}
+                <section className="col-7 text-center">
+                    <h2>Test Data Source Connection</h2>
+                    <div className="d-flex bd-highlight">
+                        <form onSubmit={handleSubmit(handleTestConnection)}>
+                            <div className="m-4 d-flex">
+                                <label className="bd-highlight">Name</label>
+                                <input className="ms-auto"
+                                    {...register("name", { required: true })}
+                                    type="text"
+                                    placeholder="Data Source Name"
+                                />
+                            </div>
+                            <div className="m-4 d-flex">
+                                <label className="bd-highlight">Type</label>
+                                <select className="ms-auto" {...register("type", { required: true })}>
+                                    <option value="POSTGRES">PostgreSQL</option>
+                                    <option value="MYSQL">MySQL</option>
+                                    <option value="SQLITE">SQLite</option>
+                                </select>
+                            </div>
+
+                            <div className="m-4 d-flex">
+                                <label className="bd-highlight">Host</label>
+                                <input className="ms-auto"
+                                    {...register("credentials.host", { required: true })}
+                                    type="text"
+                                    placeholder="Host"
+                                />
+                            </div>
+                            <div className="m-4 d-flex">
+                                <label className="bd-highlight">Port</label>
+                                <input className="ms-auto"
+                                    {...register("credentials.port", { required: true })}
+                                    type="number"
+                                    placeholder="Port"
+                                />
+                            </div>
+                            <div className="m-4 d-flex">
+                                <label className="bd-highlight">Description</label>
+                                <input className="ms-auto"
+                                    {...register("description", { required: true })}
+                                    type="text"
+                                    placeholder="Description"
+                                />
+                            </div>
+                            <div className="m-4 d-flex">
+                                <label className="bd-highlight">Database</label>
+                                <input className="ms-auto"
+                                    {...register("credentials.database", { required: true })}
+                                    type="text"
+                                    placeholder="Database"
+                                />
+                            </div>
+                            <div className="m-4 d-flex">
+                                <label className=" bd-highlight">Username</label>
+                                <input className="ms-auto"
+                                    {...register("credentials.user", { required: true })}
+                                    type="text"
+                                    placeholder="Username"
+                                />
+                            </div>
+                            <div className="m-4 d-flex">
+                                <label className=" bd-highlight">Password</label>
+                                <input className="ms-auto"
+                                    {...register("credentials.password", { required: true })}
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            </div>
+
+                            {/* Buttons for Different Actions */}
+                            <div className="button-group m-4">
+                                <button className="m-2" type="button" onClick={handleCancel}>
+                                    Cancel
+                                </button>
+                                <button className="m-2" type="button" onClick={handleSubmit(handleStoreConnection)}>
+                                    Store
+                                </button>
+                                <button className="m-2" type="button" onClick={handleSubmit(handleTestConnection)}>
+                                    Test
+                                </button>
+                                <button className="m-2" type="button" onClick={handleConnect}>
+                                    Connect
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </section>
-
-            {/* Query Form */}
-            <section>
-                <h2>Run a Query</h2>
-                {selectedDataSource ? (
-                    <form onSubmit={handleSubmit(handleQuery)}>
-                        <div>
-                            <label>SQL Query</label>
-                            <textarea
-                                {...register("query", { required: true })}
-                                placeholder="Write your SQL query here..."
-                            ></textarea>
-                        </div>
-                        <button type="submit">Run Query</button>
-                    </form>
-                ) : (
-                    <p>Please select a data source to run a query.</p>
-                )}
-            </section>
-
-            {/* Query Results */}
-            {queryResult && (
-                <section>
-                    <h2>Query Results</h2>
-                    <pre>{JSON.stringify(queryResult, null, 2)}</pre>
                 </section>
-            )}
+                <section className="col-5 ">
+                    <h2>Available Data Sources</h2>
+                    <ul className="list-unstyled">
+                        {dataSources.map((ds) => (
+                            <li key={ds.id} className="d-flex bd-highlight">
+                                <div className="text-start flex-grow-1 bd-highlight">{ds.name} ({ds.type})</div>
+                                <button
+                                    onClick={() => setSelectedDataSource(ds.id)}
+                                    className={selectedDataSource === ds.id ? "m-2 btn btn-primary" : "m-2 btn btn-secondary"}
+                                >
+                                    Select
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+                {/* Query Form */}
+                <h1 className="m-2">Run a Query</h1>
+                <section className="col-12 d-flex justify-content-center">
+
+                    {selectedDataSource ? (
+                        <form onSubmit={handleSubmit(handleQuery)}>
+                            <div>
+
+                                <textarea className="m-3" style={{
+                                    minWidth: "700px",
+                                    maxWidth: "100%",
+                                    minHeight: "50px",
+                                    height: "100%",
+                                    width: "100%",
+                                }}
+                                    {...register("query", { required: true })}
+                                    placeholder="Write your SQL query here..."
+                                ></textarea>
+                            </div>
+                            <button type="submit">Run Query</button>
+                        </form>
+                    ) : (
+                        <p>Please select a data source to run a query.</p>
+                    )}
+                </section>
+
+                {/* Query Results */}
+                {queryResult && (
+                    <section>
+                        <h2>Query Results</h2>
+                        <pre>{JSON.stringify(queryResult, null, 2)}</pre>
+                    </section>
+                )}
+            </div>
         </div>
     );
 };
