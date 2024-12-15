1c8dea62a73cc011264d8b91e8d65c090329726c
Danylo Svyryd <dantessvirid@gmail.com>
2024-12-14 22:56:04 -0400

    basic connections list

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,37 +1,37 @@
-import * as React from 'react';
-import GlobalStyles from '@mui/joy/GlobalStyles';
-import Avatar from '@mui/joy/Avatar';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import Chip from '@mui/joy/Chip';
-import Divider from '@mui/joy/Divider';
-import IconButton from '@mui/joy/IconButton';
-import Input from '@mui/joy/Input';
-import LinearProgress from '@mui/joy/LinearProgress';
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import ListItemButton, { listItemButtonClasses } from '@mui/joy/ListItemButton';
-import ListItemContent from '@mui/joy/ListItemContent';
-import Typography from '@mui/joy/Typography';
-import Sheet from '@mui/joy/Sheet';
-import Stack from '@mui/joy/Stack';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
-import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
-import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
-import QuestionAnswerRoundedIcon from '@mui/icons-material/QuestionAnswerRounded';
-import GroupRoundedIcon from '@mui/icons-material/GroupRounded';
-import SupportRoundedIcon from '@mui/icons-material/SupportRounded';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import BrightnessAutoRoundedIcon from '@mui/icons-material/BrightnessAutoRounded';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import * as React from "react";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import Avatar from "@mui/joy/Avatar";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Card from "@mui/joy/Card";
+import Chip from "@mui/joy/Chip";
+import Divider from "@mui/joy/Divider";
+import IconButton from "@mui/joy/IconButton";
+import Input from "@mui/joy/Input";
+import LinearProgress from "@mui/joy/LinearProgress";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton, { listItemButtonClasses } from "@mui/joy/ListItemButton";
+import ListItemContent from "@mui/joy/ListItemContent";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
+import Stack from "@mui/joy/Stack";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import AssignmentRoundedIcon from "@mui/icons-material/AssignmentRounded";
+import QuestionAnswerRoundedIcon from "@mui/icons-material/QuestionAnswerRounded";
+import GroupRoundedIcon from "@mui/icons-material/GroupRounded";
+import SupportRoundedIcon from "@mui/icons-material/SupportRounded";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import BrightnessAutoRoundedIcon from "@mui/icons-material/BrightnessAutoRounded";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-import ColorSchemeToggle from './ColorSchemeToggle';
-import { closeSidebar } from '../utils';
+import ColorSchemeToggle from "./ColorSchemeToggle";
+import { closeSidebar } from "../utils";
 
 function Toggler({
   defaultExpanded = false,
@@ -52,13 +52,13 @@ function Toggler({
       <Box
         sx={[
           {
-            display: 'grid',
-            transition: '0.2s ease',
-            '& > *': {
-              overflow: 'hidden',
+            display: "grid",
+            transition: "0.2s ease",
+            "& > *": {
+              overflow: "hidden",
             },
           },
-          open ? { gridTemplateRows: '1fr' } : { gridTemplateRows: '0fr' },
+          open ? { gridTemplateRows: "1fr" } : { gridTemplateRows: "0fr" },
         ]}
       >
         {children}
@@ -72,31 +72,31 @@ export default function Sidebar() {
     <Sheet
       className="Sidebar"
       sx={{
-        position: { xs: 'fixed', md: 'sticky' },
+        position: { xs: "fixed", md: "sticky" },
         transform: {
-          xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1)))',
-          md: 'none',
+          xs: "translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1)))",
+          md: "none",
         },
-        transition: 'transform 0.4s, width 0.4s',
+        transition: "transform 0.4s, width 0.4s",
         zIndex: 10000,
-        height: '100dvh',
-        width: 'var(--Sidebar-width)',
+        height: "100dvh",
+        width: "var(--Sidebar-width)",
         top: 0,
         p: 2,
         flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
+        display: "flex",
+        flexDirection: "column",
         gap: 2,
-        borderRight: '1px solid',
-        borderColor: 'divider',
+        borderRight: "1px solid",
+        borderColor: "divider",
       }}
     >
       <GlobalStyles
         styles={(theme) => ({
-          ':root': {
-            '--Sidebar-width': '220px',
-            [theme.breakpoints.up('lg')]: {
-              '--Sidebar-width': '240px',
+          ":root": {
+            "--Sidebar-width": "220px",
+            [theme.breakpoints.up("lg")]: {
+              "--Sidebar-width": "240px",
             },
           },
         })}
@@ -104,37 +104,41 @@ export default function Sidebar() {
       <Box
         className="Sidebar-overlay"
         sx={{
-          position: 'fixed',
+          position: "fixed",
           zIndex: 9998,
           top: 0,
           left: 0,
-          width: '100vw',
-          height: '100vh',
-          opacity: 'var(--SideNavigation-slideIn)',
-          backgroundColor: 'var(--joy-palette-background-backdrop)',
-          transition: 'opacity 0.4s',
+          width: "100vw",
+          height: "100vh",
+          opacity: "var(--SideNavigation-slideIn)",
+          backgroundColor: "var(--joy-palette-background-backdrop)",
+          transition: "opacity 0.4s",
           transform: {
-            xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))',
-            lg: 'translateX(-100%)',
+            xs: "translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))",
+            lg: "translateX(-100%)",
           },
         }}
         onClick={() => closeSidebar()}
       />
-      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
         <IconButton variant="soft" color="primary" size="sm">
           <BrightnessAutoRoundedIcon />
         </IconButton>
         <Typography level="title-lg">DataSmith</Typography>
-        <ColorSchemeToggle sx={{ ml: 'auto' }} />
+        <ColorSchemeToggle sx={{ ml: "auto" }} />
       </Box>
-      <Input size="sm" startDecorator={<SearchRoundedIcon />} placeholder="Search" />
+      <Input
+        size="sm"
+        startDecorator={<SearchRoundedIcon />}
+        placeholder="Search"
+      />
       <Box
         sx={{
           minHeight: 0,
-          overflow: 'hidden auto',
+          overflow: "hidden auto",
           flexGrow: 1,
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
           [`& .${listItemButtonClasses.root}`]: {
             gap: 1.5,
           },
@@ -144,8 +148,8 @@ export default function Sidebar() {
           size="sm"
           sx={{
             gap: 1,
-            '--List-nestedInsetStart': '30px',
-            '--ListItem-radius': (theme) => theme.vars.radius.sm,
+            "--List-nestedInsetStart": "30px",
+            "--ListItem-radius": (theme) => theme.vars.radius.sm,
           }}
         >
           <ListItem>
@@ -196,10 +200,10 @@ export default function Sidebar() {
                     sx={[
                       open
                         ? {
-                            transform: 'rotate(180deg)',
+                            transform: "rotate(180deg)",
                           }
                         : {
-                            transform: 'none',
+                            transform: "none",
                           },
                     ]}
                   />
@@ -249,10 +253,10 @@ export default function Sidebar() {
                     sx={[
                       open
                         ? {
-                            transform: 'rotate(180deg)',
+                            transform: "rotate(180deg)",
                           }
                         : {
-                            transform: 'none',
+                            transform: "none",
                           },
                     ]}
                   />
@@ -282,10 +286,10 @@ export default function Sidebar() {
         <List
           size="sm"
           sx={{
-            mt: 'auto',
+            mt: "auto",
             flexGrow: 0,
-            '--ListItem-radius': (theme) => theme.vars.radius.sm,
-            '--List-gap': '8px',
+            "--ListItem-radius": (theme) => theme.vars.radius.sm,
+            "--List-gap": "8px",
             mb: 2,
           }}
         >
@@ -307,11 +311,11 @@ export default function Sidebar() {
           variant="soft"
           color="warning"
           size="sm"
-          sx={{ boxShadow: 'none' }}
+          sx={{ boxShadow: "none" }}
         >
           <Stack
             direction="row"
-            sx={{ justifyContent: 'space-between', alignItems: 'center' }}
+            sx={{ justifyContent: "space-between", alignItems: "center" }}
           >
             <Typography level="title-sm">Used space</Typography>
             <IconButton size="sm">
@@ -321,14 +325,19 @@ export default function Sidebar() {
           <Typography level="body-xs">
             Your team has used 80% of your available space. Need more?
           </Typography>
-          <LinearProgress variant="outlined" value={80} determinate sx={{ my: 1 }} />
+          <LinearProgress
+            variant="outlined"
+            value={80}
+            determinate
+            sx={{ my: 1 }}
+          />
           <Button size="sm" variant="solid">
             Upgrade plan
           </Button>
         </Card>
       </Box>
       <Divider />
-      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
         <Avatar
           variant="outlined"
           size="sm"
---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosInstance } from "axios";
+
+const httpfetch: AxiosInstance = axios.create({
+  baseURL: "https://localhost/api",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json", // Default content type
+    Accept: "application/json", // Default accept header
+  },
+});
+
+const getToken = () => {
+  return localStorage.getItem("token") || "";
+};
+
+httpfetch.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    const auth = token ? `Bearer ${token}` : "";
+    config.headers["Authorization"] = auth;
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default httpfetch;
