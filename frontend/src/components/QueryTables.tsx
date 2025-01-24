171faa301c4a9597fc2862942e515d98c74e40b0
Danylo Svyryd <dantessvirid@gmail.com>
2025-01-24 22:14:37 +0200

    pagination is removed, the scroll methods is in progress

--- frontend/src/components/QueryTables.tsx
+++ frontend/src/components/QueryTables.tsx
@@ -4,9 +4,7 @@ import {
   Table,
   TableBody,
   TableCell,
-  TableContainer,
   TableHead,
-  TablePagination,
   TableRow,
   TableSortLabel,
   Button
@@ -48,19 +46,6 @@ export default function DynamicTable() {
     zIndex: isHeader ? 1 : "inherit",
     cursor: 'pointer'
   });
-
-  // const getIndexCellStyles = (isHeader: boolean = false) => ({
-  //   width: '50px',
-  //   minWidth: '50px',
-  //   maxWidth: '50px',
-  //   padding: '6px',
-  //   textAlign: 'center',
-  //   border: "1px solid gray",
-  //   backgroundColor: isHeader ? "#f5f5f5" : "#f5f5f5",
-  //   position: isHeader ? "sticky" : "inherit",
-  //   top: isHeader ? 0 : "inherit",
-  //   zIndex: isHeader ? 1 : "inherit",
-  // });
 
   const getIndexCellStyles = (isHeader: boolean = false) => ({
     width: '50px',
@@ -200,52 +185,28 @@ export default function DynamicTable() {
     const indexes = []
     for (let i = 1; i <= data.length; i++) {
       indexes.push(
-        <TableRow>
-          <TableCell align="center">
+        <TableRow key={i}>
+          <TableCell sx={getIndexCellStyles()}>
             {i}
           </TableCell>
         </TableRow>
       )
     }
     return (
-      <TableContainer>
-        <Table size="small">
           <TableBody>
             {indexes}
           </TableBody>
-        </Table>
-      </TableContainer>
     )
   }
 
-  // const renderIndexes = () => {
-  //   const indexes = headers.map((_, index) => (
-  //     <TableRow key={index}>
-  //       <TableCell align="center">
-  //         {index + 1}
-  //       </TableCell>
-  //     </TableRow>
-  //   ));
-  
-  //   return (
-  //     <TableContainer>
-  //       <Table size="small">
-  //         <TableBody>
-  //           {indexes}
-  //         </TableBody>
-  //       </Table>
-  //     </TableContainer>
-  //   );
-  // };
-
   const renderIndexesMemo = useMemo(() => renderIndexes(), [data]);
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'row', width: "100%"}}>
       {tabs != null && tab ? (
         <>
-           <JoinsSidebar tab={tab} tabs={tabs} setTabs={setTabs} openedColumns={tab.openedColumns} />
-
-         </>
+          <JoinsSidebar tab={tab} tabs={tabs} setTabs={setTabs} openedColumns={tab.openedColumns} />
+        </>
       ) : activeTab}
 
       {data.length > 0 ? (
@@ -262,160 +223,80 @@ export default function DynamicTable() {
             }}
             //onScroll={handleScroll}
           >
-          {/* <TableContainer> */}
-            {/*<Table aria-labelledby="tableTitle" size="small">*/}
-
-
-
-            {/*<TableRow sx={{ height: '53px' }}>*/}
-
-
-
-              {/* The table with indexes */}
-              <Table size="small" sx={{ width: 'auto', flexShrink: 0 }}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell sx={getIndexCellStyles(true)}>
-                      <Button 
-                        onClick={handleOpenColumns}
+            {/* The table with indexes */}
+            <Table size="small" sx={{ width: 'auto', flexShrink: 0 }}>
+              <TableHead> {/*Arrow button*/}
+                <TableRow>
+                  <TableCell sx={getIndexCellStyles(true)}>
+                    <Button 
+                      onClick={handleOpenColumns}
+                      sx={{
+                        minWidth: 'unset',
+                        padding: 0,
+                        width: '100%',
+                        height: '100%',
+                      }}
+                    >
+                      <KeyboardDoubleArrowRightIcon
                         sx={{
-                          minWidth: 'unset',
-                          padding: 0,
-                          width: '100%',
-                          height: '100%',
-                        
+                          rotate: tabs[activeTab].openedColumns ? "0deg" : "180deg",
+                          transition: "rotate 0.1s ease-in-out"
+                        }}
+                      />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              {renderIndexesMemo}
+            </Table>
+
+            {/* Query Table */}
+            <Table aria-labelledby="tableTitle" size="small">
+              {/* Static Table Header */}
+              <TableHead>
+                <TableRow>
+                  {headers.map((header) => (
+                    <TableCell
+                      key={header}
+                      sortDirection={orderBy === header ? order : false}
+                      sx={{...getQueryTableCellStyles(header, true), fontWeight: "bold"}}
+                      onClick={() => handleColumnClick(header)}
+                    >
+                      <TableSortLabel
+                        active={orderBy === header}
+                        direction={orderBy === header ? order : "asc"}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleRequestSort(header);
                         }}
                       >
-                        <KeyboardDoubleArrowRightIcon
-                          sx={{
-                            rotate: tabs[activeTab].openedColumns ? "0deg" : "180deg",
-                            transition: "rotate 0.1s ease-in-out"
-                          }}
-                        />
-                      </Button>
+                        {header}
+                      </TableSortLabel>
                     </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map((_, index) => (
-                    <TableRow key={index}>
-                      <TableCell sx={getIndexCellStyles()}>
-                        {index + 1}
-                      </TableCell>
-                    </TableRow>
                   ))}
-                </TableBody>
-   {/*             <TableHead>
-   <TableRow>
-    <TableCell sx={getIndexCellStyles(true)}>
-      <Button onClick={handleOpenColumns}>
-        <KeyboardDoubleArrowRightIcon />
-      </Button>
-    </TableCell>
-  </TableRow>
-</TableHead>
-<TableBody>
-  {data.map((_, index) => (
-    <TableRow key={index}>
-      <TableCell sx={getIndexCellStyles()}>
-        {index + 1}
-      </TableCell>
-    </TableRow>
-  ))}
-</TableBody> */}
-              </Table>
+                </TableRow>
+              </TableHead>
 
-
-
-
-
-
-
-
-
-
-
-              <Table size="small">
-                {/* Static Table Header */}
-                <TableHead>
-                  <TableRow>
+              {/* Dynamic Table Body */}
+              <TableBody>
+                {visibleRows.map((row, index) => (
+                  <TableRow hover tabIndex={-1} key={index}>
                     {headers.map((header) => (
                       <TableCell
                         key={header}
-                        sortDirection={orderBy === header ? order : false}
-                        sx={{...getQueryTableCellStyles(header, true), fontWeight: "bold"}}
-                        onClick={() => handleColumnClick(header)}
+                        sx={getQueryTableCellStyles(header)}
+                        title={row[header]}
+                        onClick={(e) => {
+                          handleColumnClick(header);
+                        }}
                       >
-                        <TableSortLabel
-                          active={orderBy === header}
-                          direction={orderBy === header ? order : "asc"}
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleRequestSort(header);
-                          }}
-                        >
-                          {header}
-                        </TableSortLabel>
+                        {row[header] != null ? row[header] : "NULL"}
                       </TableCell>
                     ))}
                   </TableRow>
-                </TableHead>
-
-                {/* Dynamic Table Body */}
-                <TableBody>
-                  {visibleRows.map((row, index) => (
-                    <TableRow hover tabIndex={-1} key={index}>
-                      {/*{headers.map((header) => (
-                        <TableCell
-                          key={header}
-                          sx={getQueryTableCellStyles(header)}
-                          title={row[header]}
-                          onClick={(e) => {
-                            handleColumnClick(header);
-                          }}
-                        >
-                          {row[header] != null ? row[header] : "NULL"}
-                        </TableCell>
-                      ))}*/}
-                      {headers.map((header) => {
-                      const cellContent = row[header]?.toString() || '';
-                      return (
-                        <TableCell
-                          //key={`${index}-${header}`}
-                          key={header}
-                          sx={getQueryTableCellStyles(header)}
-                          title={cellContent || undefined}
-                          //title={row[header]}
-                          onClick={() => handleColumnClick(header)}
-                        >
-                          {cellContent}
-                        </TableCell>
-                      );
-                    })}
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-              
-            {/*</Table>*/}
-          {/* </TableContainer> */}
+                ))}
+              </TableBody>
+            </Table>
           </Box>
         ) :
         <Box sx={{display: "flex", width: "100%", border: "1px solid gray"}}>
