90fd2389b22a26d8b5dd3944880d1dbe74a4937b
Danylo Svyryd <dantessvirid@gmail.com>
2025-03-08 20:52:45 -0400

    database engine

--- backend/datasmith/settings.py
+++ backend/datasmith/settings.py
@@ -104,25 +104,37 @@ WSGI_APPLICATION = "datasmith.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE", ""),
-        "NAME": os.getenv("DB_NAME", ""),
-        "USER": os.getenv("DB_USER", ""),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", ""),
-        "PORT": os.getenv("DB_PORT", ""),
-    },
-    "test": {
-        "ENGINE": os.getenv("DB_ENGINE", ""),
-        "NAME": os.getenv("test_db", ""),
-        "USER": os.getenv("DB_USER", ""),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", ""),
-        "PORT": os.getenv("DB_PORT", ""),
-    },
-}
 
+DATABASES = (
+    (
+        {
+            "default": {
+                "ENGINE": os.getenv("DB_ENGINE", ""),
+                "NAME": os.getenv("DB_NAME", ""),
+                "USER": os.getenv("DB_USER", ""),
+                "PASSWORD": os.getenv("DB_PASSWORD", ""),
+                "HOST": os.getenv("DB_HOST", ""),
+                "PORT": os.getenv("DB_PORT", ""),
+            },
+            "test": {
+                "ENGINE": os.getenv("DB_ENGINE", ""),
+                "NAME": os.getenv("test_db", ""),
+                "USER": os.getenv("DB_USER", ""),
+                "PASSWORD": os.getenv("DB_PASSWORD", ""),
+                "HOST": os.getenv("DB_HOST", ""),
+                "PORT": os.getenv("DB_PORT", ""),
+            },
+        }
+    )
+    if os.getenv("DOCKER") == "True"
+    else (
+        {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+            },
+        }
+    )
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
